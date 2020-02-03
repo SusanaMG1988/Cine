5abe52f7e0a51d2233de7c55583000e12cb6daf4
susifu <susanamg1988@hotmail.com>
2020-02-03 21:15:20 +0100

    hghg

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CINE</title>
+    <title>CINEs</title>
 </head>
 <body>
     <?php 
@@ -15,7 +15,7 @@
     require_once 'Espectadores.php';
     $peli = new Pelicula("Lo que el código se llevó",120 ,89 , "Pepito Grillo");
     //$cine = new Butaca();
-
+ 
     //Crea una función que imprima una tabla html que en cada celda aparezca la fila y la columna de la butaca 
     //la fila y la columna de la butaca 
     function printButacas($c){
