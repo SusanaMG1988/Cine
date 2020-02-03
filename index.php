1cb7e0858d8ffc791d352df63221be8c732268f4
susifu <susanamg1988@hotmail.com>
2020-02-03 21:14:06 +0100

    cambios uno

--- index.php
+++ index.php
@@ -9,10 +9,29 @@
 <body>
     <?php 
     
-    require_once 'peliculas.php';
-    require_once 'cine.php';
+    require_once 'Pelicula.php';
+    require_once 'Cine.php';
+    require_once 'Butaca.php';
+    require_once 'Espectadores.php';
     $peli = new Pelicula("Lo que el código se llevó",120 ,89 , "Pepito Grillo");
     //$cine = new Butaca();
+
+    //Crea una función que imprima una tabla html que en cada celda aparezca la fila y la columna de la butaca 
+    //la fila y la columna de la butaca 
+    function printButacas($c){
+        $arrayButacas = $c->getButacas();
+
+    echo "<table>";
+    for($i=0; $i < $c->getFilas(); $i++){
+        echo "<tr>";
+        for($j=0; $j < $c->getLEtra(); $j++){
+            $butaca = $arrayButacas;
+            echo "<td>{$butaca->getFila()},{$butaca->getLetra()}</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    }
     ?>
 
 </body>
